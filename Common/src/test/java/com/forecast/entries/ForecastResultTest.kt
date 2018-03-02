62039a3adb13a7773a82e610e8a942efd1dbe768
glusan <andrey.glushok@f-secure.com>
2018-03-02 12:55:11 +0300

    test fix

--- Common/src/test/java/com/forecast/entries/ForecastResultTest.kt
+++ Common/src/test/java/com/forecast/entries/ForecastResultTest.kt
@@ -7,13 +7,12 @@ internal class ForecastResultTest {
     @Test
     internal fun computeScore() {
         val match = Match(Team("a"), Team("b"))
-        assertThat(ForecastResult(Forecast(match, 1, 1), Forecast(match, 1, 1)).computeScore()).isEqualTo(4);
-        assertThat(ForecastResult(Forecast(match, 1, 1), Forecast(match, 0, 0)).computeScore()).isEqualTo(3);
-        assertThat(ForecastResult(Forecast(match, 1, 1), Forecast(match, 1, 2)).computeScore()).isEqualTo(0);
-        assertThat(ForecastResult(Forecast(match, 2, 1), Forecast(match, 1, 2)).computeScore()).isEqualTo(0);
-        assertThat(ForecastResult(Forecast(match, 2, 1), Forecast(match, 2, 1)).computeScore()).isEqualTo(4);
-        assertThat(ForecastResult(Forecast(match, 2, 1), Forecast(match, 3, 1)).computeScore()).isEqualTo(2);
-        assertThat(ForecastResult(Forecast(match, 2, 1), Forecast(match, 3, 2)).computeScore()).isEqualTo(3);
-        assertThat(ForecastResult(Forecast(match, 1, 2), Forecast(match, 2, 3)).computeScore()).isEqualTo(3);
+        assertThat(ForecastResult(match, Forecast(1, 2), Forecast(1, 2)).computeScore()).isEqualTo(4);
+        assertThat(ForecastResult(match, Forecast(1, 1), Forecast(0, 0)).computeScore()).isEqualTo(3);
+        assertThat(ForecastResult(match, Forecast(2, 1), Forecast(1, 2)).computeScore()).isEqualTo(0);
+        assertThat(ForecastResult(match, Forecast(2, 1), Forecast(2, 1)).computeScore()).isEqualTo(4);
+        assertThat(ForecastResult(match, Forecast(2, 1), Forecast(3, 1)).computeScore()).isEqualTo(2);
+        assertThat(ForecastResult(match, Forecast(2, 1), Forecast(3, 2)).computeScore()).isEqualTo(3);
+        assertThat(ForecastResult(match, Forecast(1, 2), Forecast(2, 3)).computeScore()).isEqualTo(3);
     }
 }
