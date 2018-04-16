9fe7b574556c95dca555d2ae31cce89526277847
glusan <andrey.glushok@f-secure.com>
2018-04-16 12:30:24 +0300

    console save or not dialog

--- Console/src/main/java/com/forecast/ForecastProcessor.java
+++ Console/src/main/java/com/forecast/ForecastProcessor.java
@@ -2,14 +2,14 @@ package com.forecast;
 
 import javax.swing.*;
 
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+import static javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE;
 
 public class ForecastProcessor {
 
     public static void main(String[] args) {
         try {
             JFrame frame = new MainFrame();
-            frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            frame.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
             frame.setVisible(true);
         } catch (Exception e) {
             System.out.println(e.getMessage());
--- Console/src/main/java/com/forecast/MainFrame.java
+++ Console/src/main/java/com/forecast/MainFrame.java
@@ -4,9 +4,13 @@ import com.forecast.forecasts.ForecastPanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.UnsupportedEncodingException;
 
 import static com.forecast.resources.ResourceUtils.getString;
+import static javax.swing.JOptionPane.CANCEL_OPTION;
+import static javax.swing.JOptionPane.showConfirmDialog;
 
 public class MainFrame extends JFrame {
 
@@ -14,6 +18,22 @@ public class MainFrame extends JFrame {
         super("Forecast Processor");
         setPreferredSize(new Dimension(1000, 700));
         setMinimumSize(new Dimension(1000, 700));
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int closeOption = showConfirmDialog(MainFrame.this, getString("confirmClosing"));
+                switch (closeOption) {
+                    case JOptionPane.OK_OPTION:
+                        e.getWindow().dispose();
+                        break;
+                    case JOptionPane.NO_OPTION:
+                        e.getWindow().dispose();
+                        break;
+                    case CANCEL_OPTION:
+                        break;
+                }
+            }
+        });
         initTabbedPane();
     }
 
